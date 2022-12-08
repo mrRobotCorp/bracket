b031fd1f8479eda196c65ce82fa4c4a0f9b8e1c8
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2022-12-09 00:40:07 +0900

    .
    .

--- js/home.js
+++ js/home.js
@@ -22,13 +22,13 @@ document.querySelector('.overview_iam').insertAdjacentHTML('beforeend', `
 // ------------------------- keyword scrollTrigger -----------------------------------
 gsap.registerPlugin(ScrollTrigger);
 
-const showDemo = () => {
+const showTxt = () => {
   document.body.style.overflow = 'auto';
   document.scrollingElement.scrollTo(0, 0);
 
   gsap.utils.toArray('section').forEach((section, index) => {
     const w = section.querySelectorAll('.wrapper');
-    const [x, xEnd] = index % 2 ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+    const [x, xEnd] = index % 2 ? ['100%', 0] : ['-100%', 0];
     gsap.fromTo(w, { x }, {
       x: xEnd,
       scrollTrigger: {
@@ -37,7 +37,7 @@ const showDemo = () => {
   });
   };
 
-showDemo();
+showTxt();
 
 
 // ----------------- thumbnail img change --------------------------
