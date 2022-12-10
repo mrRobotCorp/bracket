7daecb78b533cbde5c5c9571d52831d9836435fd
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2022-12-10 18:56:35 +0900

    .
    .

--- js/study.js
+++ js/study.js
@@ -5,7 +5,7 @@ $(function(){
         bodyHeight = $(".js-target").height(),
             scrollTop = $(document).scrollTop();
         rate = scrollTop/bodyHeight+0.9763033175;
-        grade = "linear-gradient("+(rate*45)+"deg, rgb(251, 189, 54) 0%, rgba(157,237,244,0) 70%),linear-gradient("+(rate*135)+"deg, rgb(255, 248, 81) 10%, rgba(235,170,236,0) 80%),linear-gradient("+(rate*225)+"deg, rgb(245, 153, 66) 10%, rgba(170,236,170,0) 80%),linear-gradient("+(rate*315)+"deg, rgb(83, 68, 255) 100%, rgba(244,234,122,0) 70%)";
+        grade = "linear-gradient("+(rate*45)+"deg, rgb(236, 70, 54) 0%, rgba(157,237,244,0) 70%),linear-gradient("+(rate*135)+"deg, rgb(255, 248, 81) 10%, rgba(235,170,236,0) 80%),linear-gradient("+(rate*225)+"deg, rgb(245, 153, 66) 10%, rgba(170,236,170,0) 80%),linear-gradient("+(rate*315)+"deg, rgb(83, 68, 255) 100%, rgba(244,234,122,0) 70%)";
         gradation.$content.css({
             background: grade
         });
