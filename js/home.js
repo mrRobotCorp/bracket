fb586bfb828ad7061265775a4d9fe7649ef4ec99
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2022-12-12 16:34:42 +0900

    .
    .

--- js/home.js
+++ js/home.js
@@ -49,7 +49,6 @@ $(function() {
   });
   
   $.stellar();
-  
   $('.main').addClass('active');
 });
 
--- js/project.js
+++ js/project.js
@@ -1,9 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let SECTIONS = gsap.utils.toArray("section");
+let sections = gsap.utils.toArray("section");
 
-gsap.to(SECTIONS, {
-	xPercent: -100 * ( SECTIONS.length -2 ),
+gsap.to(sections, {
+	xPercent: -100 * ( sections.length -2 ),
 	ease: "none",
 	scrollTrigger: {
 		trigger: '#container',
@@ -11,4 +11,28 @@ gsap.to(SECTIONS, {
 		pin: true,
 		scrub: 0.5,
 	}
+});
+
+// ----------------- thumbnail img change --------------------------
+const thumbnail = document.querySelectorAll(".thumbnail");
+
+thumbnail.forEach(function (thumb) {
+    thumb.addEventListener("mouseover", function () {
+        cursor.classList.add("projectHover");
+        follower.classList.remove("cursor_follower");
+        function myCursor(e) {
+            cursor.style.top = e.pageY + 45 + "px";
+            cursor.style.left = e.pageX + 45 + "px";
+        }
+        window.addEventListener("mousemove", myCursor);
+    });
+    thumb.addEventListener("mouseleave", function () {
+        cursor.classList.remove("projectHover");
+        follower.classList.add("cursor_follower");
+        function myCursor(e) {
+            cursor.style.top = e.pageY + 10 + "px";
+            cursor.style.left = e.pageX + 10 + "px";
+        }
+        window.addEventListener("mousemove", myCursor);
+    });
 });
