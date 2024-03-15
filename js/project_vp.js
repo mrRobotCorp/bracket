bae55ea8deeea6922f96a160945661cc61b29726
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2024-03-15 16:16:51 +0900

    .
    .

--- js/project_vp.js
+++ js/project_vp.js
@@ -40,28 +40,32 @@ const updatePinPosition = time => {
 requestAnimationFrame(updatePinPosition);
 
 // ------------------ img hover custom mouse ------------------
-const thumbnail = document.querySelectorAll(".pro_thumbnail");
+if(window.innerWidth > 768) {
+    const thumbnail = document.querySelectorAll(".pro_thumbnail");
+    
+    thumbnail.forEach(function (thumb) {
+        thumb.addEventListener("mouseover", function () {
+            cursor.classList.add("thumbHover");
+            follower.classList.remove("cursor_follower");
+            function myCursor(e) {
+                cursor.style.top = e.pageY + 60 + "px";
+                cursor.style.left = e.pageX + 60 + "px";
+            }
+            window.addEventListener("mousemove", myCursor);
+        });
+        thumb.addEventListener("mouseleave", function () {
+            cursor.classList.remove("thumbHover");
+            follower.classList.add("cursor_follower");
+            function myCursor(e) {
+                cursor.style.top = e.pageY + 10 + "px";
+                cursor.style.left = e.pageX + 10 + "px";
+            }
+            window.addEventListener("mousemove", myCursor);
+        });
+    });
 
-thumbnail.forEach(function (thumb) {
-    thumb.addEventListener("mouseover", function () {
-        cursor.classList.add("thumbHover");
-        follower.classList.remove("cursor_follower");
-        function myCursor(e) {
-            cursor.style.top = e.pageY + 60 + "px";
-            cursor.style.left = e.pageX + 60 + "px";
-        }
-        window.addEventListener("mousemove", myCursor);
-    });
-    thumb.addEventListener("mouseleave", function () {
-        cursor.classList.remove("thumbHover");
-        follower.classList.add("cursor_follower");
-        function myCursor(e) {
-            cursor.style.top = e.pageY + 10 + "px";
-            cursor.style.left = e.pageX + 10 + "px";
-        }
-        window.addEventListener("mousemove", myCursor);
-    });
-});
+}
+
 
 // ------------------------------------------
 document.querySelector('.pro_thumbnail').insertAdjacentHTML('afterend', `
